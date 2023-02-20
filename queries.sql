858dbfec8bfc1e5dcfa22b8cbe668d7072fd3eef
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-02-20 17:21:21 +0200

    Denbug the schema and queries based on code review

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-1-1';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-12-31';
 
 SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
 
--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE animals(
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(250),
-  date_of_birt DATE,
+  date_of_birth DATE,
   escape_attempts INT,
   neutered BOOL,
   weight_kg DECIMAL,
