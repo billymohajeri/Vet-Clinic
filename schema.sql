e7e7ab8b91070ade96a9b8ccb34cd2196ad733a8
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-02-23 10:40:56 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -53,3 +53,41 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_owners
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
+
+
+-- Create a table named vets with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+-- age: integer
+-- date_of_graduation: date
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+
+-- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it.
+-- Create a "join table" called specializations to handle this relationship.
+CREATE TABLE specializations (
+    vets_id INT,
+    species_id INT,
+    PRIMARY KEY (vets_id, species_id),
+    FOREIGN KEY (vets_id) REFERENCES vets(id) ON DELETE CASCADE,
+    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE
+);
+
+
+-- There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals.
+-- Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    animal_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (animal_id) REFERENCES animals(id) ON DELETE CASCADE,
+    FOREIGN KEY (vets_id) REFERENCES vets(id) ON DELETE CASCADE
+);
