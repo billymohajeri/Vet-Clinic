972480e22ea853df92c28f9cede2dbea853fb493
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-27 10:53:55 +0300

    Add analyze queries

--- queries.sql
+++ queries.sql
@@ -222,3 +222,9 @@ ON VI.vets_id = VE.id
 WHERE VE.name = 'Maisy Smith'
 GROUP BY S.name
 ORDER BY COUNT(*) DESC LIMIT 1;
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
